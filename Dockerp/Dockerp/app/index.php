b6cc100fc95ba340d94fce17e5e28181128836d8
woodjiny <103391073+woodjiny@users.noreply.github.com>
2023-11-08 11:08:50 -0400

    Add files via upload

---
+++ Dockerp/Dockerp/app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker Compose</title>
+</head>
+<body>
+
+    <h1>Docker Compose  Woodjny S Renard</h1>
+        <?php
+            $mat = getenv("MATRICULA");
+            $nombre = getenv("NAME");
+            echo "<h1>Hola Maestro, informacion contenida en mi aplicacion:</h1>
+            <p>Matricula: $mat</p>
+            <p>Nombre: $nombre </p> ";
+        ?>
+
+
+</body>
+</html>
